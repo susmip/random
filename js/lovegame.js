464c149e401477f6ddac6a0692879df9001eef9b
susmip <98744813+susmip@users.noreply.github.com>
2022-02-24 00:01:07 +0530

    Create lovegame.js

---
+++ js/lovegame.js
@@ -0,0 +1,10 @@
+var n= Math.random()
+n=n*100;
+n=Math.floor(n)+1;
+
+if(n>70){
+  console.log("your love score is "+n+" %"+" YOU ARE LIKE ROMEO AND JULIET" );
+}
+else{
+  console.log("your love score is "+n+" %"+" Love grows" );
+}
